a58673af424579e5cee31820d42b1ffcbc96a0f1
thief-g <92672068+thief-g@users.noreply.github.com>
2021-10-18 10:40:27 +0530

    Update Graph-Algorithms.cpp

--- Graph-Algorithms.cpp
+++ Graph-Algorithms.cpp
@@ -35,3 +35,33 @@ void dfs(int v) {
             dfs(u);
     }
 }
+
+// algo to find the connected components in the graph
+int n;
+vector<int> g[MAXN] ;
+bool used[MAXN] ;
+vector<int> comp ;
+
+void dfs(int v) {
+    used[v] = true ;
+    comp.push_back(v);
+    for (size_t i = 0; i < (int) g[v].size(); ++i) {
+        int to = g[v][i];
+        if (!used[to])
+            dfs(to);
+    }
+}
+
+void find_comps() {
+    for (int i = 0; i < n ; ++i)
+        used [i] = false;
+    for (int i = 0; i < n ; ++i)
+        if (!used[i]) {
+            comp.clear();
+            dfs(i);
+            cout << "Component:" ;
+            for (size_t j = 0; j < comp.size(); ++j)
+                cout << ' ' << comp[j];
+            cout << endl ;
+        }
+}
