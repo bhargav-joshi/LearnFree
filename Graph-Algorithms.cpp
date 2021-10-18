c564202d80f981d838219f30f0e385a3d86d2aee
thief-g <92672068+thief-g@users.noreply.github.com>
2021-10-18 10:36:29 +0530

    Update Graph-Algorithms.cpp

--- Graph-Algorithms.cpp
+++ Graph-Algorithms.cpp
@@ -21,3 +21,17 @@ while (!q.empty()) {
         }
     }
 }
+
+// dfs algo 
+vector<vector<int>> adj; // graph represented as an adjacency list
+int n; // number of vertices
+
+vector<bool> visited;
+
+void dfs(int v) {
+    visited[v] = true;
+    for (int u : adj[v]) {
+        if (!visited[u])
+            dfs(u);
+    }
+}
