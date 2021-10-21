375502369d42b199279d670efa0632a07e77739a
mansi-garg-12 <76842775+mansi-garg-12@users.noreply.github.com>
2021-10-21 23:28:08 +0530

    uploaded netflix website clone code
    i have made a netflix website clone.

---
+++ page1.php
@@ -0,0 +1,331 @@
+<!doctype html>
+<html>
+    <title>Netflix India-Watch Tv Shows Online Watch movies Online</title>
+    <head>
+        <link rel = "icon" href ="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Netflix_2015_N_logo.svg/1128px-Netflix_2015_N_logo.svg.png" height=10px width=10px image/x-icon">
+    <head>
+    <script language=javascript>
+   function signin()
+{
+  window.location.href="netflix12.php";
+}
+    </script>
+        <style>
+            body
+            {
+               
+               
+                background: url("https://help.nflxext.com/43e0db2f-fea0-4308-bfb9-09f2a88f6ee4_what_is_netflix_1_en.png ");
+                background-position:top;
+                background-size: 1400px 700px;
+                background-repeat: no-repeat;
+                background-color: black;
+            }
+            select
+            {
+                color:white;
+                text-decoration-color:white;
+                width:100px;
+                height:40px;
+                background-color: black;
+                border-color:white;
+                margin-left:900px;
+                font-size:16px;
+            }
+            #b
+            {
+                width:100px;
+                height:40px;
+                background-color:rgb(209, 4, 4) ;
+                color:white;
+                border-radius: 5px;
+                border-color:rgb(209, 4, 4) ;
+                text-transform:bold;
+                font-size:16px;
+            }
+            #e
+            {
+                width:500px;
+                height:55px;
+                background-color: white;
+                font-size: 20px;
+
+            }
+            #s
+            {
+                width:200px;
+                height:60px;
+                background-color: rgb(209, 4, 4) ;
+                font-size: 25px;
+                color:white;
+                border-color:rgb(209, 4, 4) ;
+            }
+            hr
+            {
+                background-color: rgb(63, 60, 60);
+                border-color:rgb(63, 60, 60) ;
+            }
+            div
+            {
+                 width:300px;
+                 height:90px;
+                 background-color:white;
+                 border-color:red;
+                 border-radius:5px;
+                 margin-left:120px;
+            }
+             #se
+              {
+             background:url("https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png");
+             height:450px;
+             width:500px;
+               }
+            #q
+            {
+                width:800px;
+                height:80px;
+                font-size:25px;
+                text-align:left;
+                color:white;
+                background-color:rgb(49, 48, 48);
+            }
+            a:link
+            {
+                color:grey;
+            }
+            a:visited
+            {
+                color:grey;
+            }
+            #v
+            {
+                background:url("https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png");
+                height:500px ;
+                width:550px;
+            }
+        </style>
+    </head>
+    <body bgcolor="black">
+        <table>
+            <tr>
+                <td>
+                    &nbsp&nbsp &nbsp&nbsp<img src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png" height=70px width=150px>
+                </td>
+                <td>
+                    <select>
+                    <option>English</option>
+                    <option>हिन्दी</option>
+                    </select>
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    <input type=button value="Sign In" onclick=signin() id=b >
+                </td>
+            </tr>
+        </table>
+        <br><br><br><br><br><br><br><br>
+        <center>
+            <font face=arial size=7 color=white>
+                <b>
+                Unlimited movies, TV<br>
+                shows and more.
+                </b>
+            </font>
+            <br><br>
+            <font face=arial size=5 color=white>
+                Watch anywhere. Cancel anytime.
+            </font>
+            <br><br>
+            <font face=arial size=4 color=white>
+                Ready to watch? Enter your email to create or restart your membership.
+            </font>
+            <br><br>
+            <table>
+                <tr>
+                    <td><input type =text placeholder ="   Email address" id=e></td>
+                    <td><input type=button value="Get Started  >" id=s></td>
+                </tr>
+            </table>
+            <br><br><br><br><br><br><br><br><br><br>
+            <hr width="full" size="7px">
+        </center>
+        <br><br><br><br>
+        <table >
+            <br><br>
+            <tr>
+                <td>
+                   <font face=arial size=7 color=white>
+                       <b>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Enjoy on your TV.</b>
+                    </font><br>
+                   <font face=arial size=5 color=white>
+                        <font face=arial size=7 color=white> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</font>
+                        Watch on smart TVs, PlayStation, Xbox,<br>
+                        <font face=arial size=7 color=white>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</font>
+                        Chromecast, Apple TV, Blu-ray players and <br>
+                        <font face=arial size=7 color=white>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</font>
+                        more.</font>
+                </td>
+                <td> &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp
+                    
+                     <div id=se>
+<br><br><br><br><br>
+                    <video  autoplay="" playsinline="" muted="" loop=""><source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-in-0819.m4v" type="video/mp4" height="100px" width="200px " id=v "></video>
+                    </div>
+</td>
+            </tr>
+        </table>
+        <br><br><br><br>
+        <hr width="full" size="7px">
+         <table><br><br>
+            <tr>
+                <td>
+                    &nbsp&nbsp&nbsp<img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" height="350px" width="500px">
+                    <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/boxshot.png">
+                    <div></div>
+                </td>
+                <td>
+                    <b>
+                        <font face=arial size=7 color=white>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspDownload your shows<br>
+                             &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspto watch offine .
+                        </font>
+                    </b><br>
+                        <font face=arial size=5 color=white>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSave your favourite easily and always have 
+                            <br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspsomething to watch.
+                         </font><br>
+                 </td>
+            </tr>
+         </table>
+         <br><br>
+         <hr width="full" size="7px">
+         <table >
+            <br><br><br>
+            <tr>
+                <td>
+                    <br><br>
+                   <font face=arial size=7 color=white>
+                       <b>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspWatch Everywhere.</b>
+                    </font><br>
+                   <font face=arial size=5 color=white>
+                        <font face=arial size=7 color=white> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</font>
+                        Stream unlimited movies and TV shows on<br>
+                        <font face=arial size=7 color=white>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</font>
+                        your phone, tablet, laptop, and TV. <br>
+                    </font>
+                </td>
+                <td> &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp
+                    <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile-in.png" height="400px" width="500px">
+                </td>
+            </tr>
+        </table>
+        <br><br>
+         <hr width="full" size="7px">
+         <table><br><br><br>
+            <tr>
+                <td align="right">
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<img src="https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABVxdX2WnFSp49eXb1do0euaj-F8upNImjofE77XStKhf5kUHG94DPlTiGYqPeYNtiox-82NWEK0Ls3CnLe3WWClGdiJP.png?r=5cf" height="350px" width="500px">
+                </td>
+                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                <td><br>
+                    <b>
+                        <font face=arial size=7 color=white>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspCreate profiles for <br>
+                             &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspchildren.
+                        </font>
+                    </b><br><br>
+                        <font face=arial size=5 color=white>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSend children on adventures with their 
+                            <br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspfavourite characters in a space made just for
+                            <br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspthem—free with your membership.
+                         </font><br>
+                 </td>
+            </tr>
+         </table>
+         <br><br><br><br>
+         <hr width="full" size="7px">
+         <br><br><br>
+         <center>
+         <font size=8 face=arial color=white>
+             <b> Frequently Asked Questions</b>
+         </font>
+         <br><br><br>
+         <input type="button"  value ="  What is Netflix?                                                                                +" id=q><br><br>
+         <input type="button"  value ="  How much does Netflix cost?                                                           +" id=q><br><br>
+         <input type="button"  value ="  Where can I watch?                                                                          +" id=q><br><br>
+         <input type="button"  value ="  How do I cancel?                                                                              +" id=q><br><br>
+         <input type="button"  value ="  What can I watch on Netflix?                                                            +" id=q><br><br>
+         <input type="button"  value ="  Is Netflix good for kids?                                                                    +" id=q><br><br>
+        <br><br><br>
+         <font face=arial size=4 color=white>
+         Ready to watch? Enter your email to create or restart your membership. <br><br> 
+        </font>
+        <table>
+            <tr>
+                <td><input type =text placeholder ="   Email address" id=e></td>
+                <td><input type=button value="Get Started  >" id=s></td>
+            </tr>
+        </table>                
+        
+        <br><br><br><br>
+         <hr width="full" size="7px">
+         <br><br><br>
+         <font face=arial size=2 color=lightgrey>
+             <br><br><br>
+             
+            <table>
+                <tr>
+                    <td colspan=4>Questions? Call <a href="tel:000-800-040-1843">000-800-040-1843</a></td>
+                </tr>
+                <tr><td colspan="4"></td></tr>
+                <tr><td colspan="4"></td></tr>
+                <tr><td colspan="4"></td></tr>
+                <tr><td colspan="4"></td></tr>
+                <tr><td colspan="4"></td></tr>
+                <tr>
+                    <td><a href="https://help.netflix.com/support/412">FAQ        </a></td>
+                    <td><a href="https://help.netflix.com/">Help Centre           </a></td>
+                    <td><a href="https://www.netflix.com/youraccount">Acccount      </a></td>
+                    <td><a href="https://media.netflix.com/">Medei Centre         </a></td>
+                </tr>
+                <tr><td colspan=4></td></tr>
+                <tr><td colspan="4"></td></tr>
+                <tr><td colspan="4"></td></tr>
+                <tr>
+                    <td><a href="http://ir.netflix.com/">Investors Relations</a></td>
+                    <td><a href="https://jobs.netflix.com/jobs">Jobs</a></td>
+                    <td><a href="https://www.netflix.com/watch">Ways To Watch</a></td>
+                    <td><a href="https://help.netflix.com/legal/termsofuse">Terms of Use</a></td>
+                </tr>
+                <tr><td colspan="4"></td></tr>
+                <tr><td colspan="4"></td></tr>
+                <tr><td colspan="4"></td></tr>
+                <tr>
+                    <td><a href="https://help.netflix.com/support/412">Privacy</a></td>
+                    <td><a href="https://help.netflix.com/">Cookie Preferences</a></td>
+                    <td><a href="https://www.netflix.com/youraccount">Coorporate Information</a></td>
+                    <td><a href="https://media.netflix.com/">Contact Us</a></td>
+                </tr>
+                <tr><td colspan="4"></td></tr>
+                <tr><td colspan="4"></td></tr>
+                <tr><td colspan="4"></td></tr>
+                <tr>
+                    <td><a href="https://help.netflix.com/support/412">Speed Testn</a></td>
+                    <td><a href="https://help.netflix.com/">Legal Notices</a></td>
+                    <td><a href="https://www.netflix.com/youraccount">Only on Netflix</a></td>
+                    
+                </tr>
+                <tr><td colspan="4"></td></tr>
+                <tr><td colspan="4"></td></tr>
+                <tr><td colspan="4"></td></tr>
+                <tr>
+                    <td colspan="4" id=t>
+                        <select>
+                        <option>English</option>
+                        <option>हिन्दी</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr><td colspan="4"></td></tr>
+                <tr><td colspan="4"></td></tr>
+                <tr><td colspan="4"></td></tr>
+                <tr><td colspan="4">Netflix India</td></tr>
+            </table>
+        </font>
+    </center>
+    </body>
+</html>
