4407f1aefb3db8813d0e61a56da597a9190b589a
Tushar Sarin <tusharsarin478@gmail.com>
2021-10-23 00:27:53 +0530

    Update twosum Problem.c

--- C-lang Codes/twosum Problem.c
+++ C-lang Codes/twosum Problem.c
@@ -1,45 +1,34 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* findAnagrams(char * s, char * p, int* returnSize)
-{
-    int ls = strlen(s);
-    int lp = strlen(p);
-    int *result;
-    int count = 0;
-    if (ls < lp)
-    {
-        result = malloc(1);
-        (* returnSize) = 0;
-        return result;
-    }
-    result = malloc(sizeof(int)*(ls-lp+1));
-    int pc[26];
-    int sc[26];
-    memset(pc,0,sizeof(int)*26);
-    memset(sc,0,sizeof(int)*26);
-    
-    int i,k;
-    for (i=0; i<(lp-1); i++)
-    {
-        pc[p[i]-'a']++;
-        sc[s[i]-'a']++;
-    }
-    pc[p[i]-'a']++;
-    for (i=0,k=lp-1; i<(ls-lp+1); i++,k++)
-    {
-        sc[s[k]-'a']++;
-        int j;
-        for (j=0; j<26; j++)
-            if (sc[j] != pc[j])
-                break;
-        
-        if (j==26)
-        {
-            result[count++] = i;
-        }
-        sc[s[i]-'a']--;
-    }
-    (* returnSize) = count;
-    return(result);
+typedef struct {
+	int num;
+	int idx;
+	// Makes structure hashable
+	UT_hash_handle hh;
+} num_t;
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+	int* res = calloc((*returnSize = 2), sizeof(int));
+	num_t *numsHash = NULL, *num = NULL, *tmp = NULL;
+
+	for(int i = 0; i < numsSize; ++i){
+		int toFind = target - nums[i];
+		HASH_FIND_INT(numsHash, &toFind, num);
+
+		if (num != NULL){
+			res[0] = num->idx;
+			res[1] = i;
+
+			break;
+		} else {
+			num = malloc(sizeof(num_t));
+			num->num = nums[i]; num->idx = i;
+
+			HASH_ADD_INT(numsHash, num, num);
+		}
+	}
+
+	HASH_ITER(hh, numsHash, num, tmp) {
+		HASH_DEL(numsHash, num); free(num); 
+	}
+
+	return res;
 }
