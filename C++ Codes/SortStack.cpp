16af137c437e99ecd97396625d81fff050a22252
sattudev <92978828+sattudev@users.noreply.github.com>
2021-10-22 14:37:53 +0530

    Stack Sort Program
    program for sorting a Stack

---
+++ C++ Codes/SortStack.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define debug(x){cout<<"x : "<<x<<endl;}
+
+
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+	//for getting input from input.txt
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int n;
+	cin >> len;
+
+
+}
+}
+
+
