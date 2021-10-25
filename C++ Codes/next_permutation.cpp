8c090b9d05e8383dde25cf981b8f85a22b04c44d
Shivam Yadav <1906505@kiit.ac.in>
2021-10-25 18:42:20 +0530

    Create next_permutation.cpp

---
+++ C++ Codes/next_permutation.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+ 
+int main()
+{
+    int arr[] = { 1, 2, 3 };
+ 
+    sort(arr, arr + 3);
+ 
+    cout << "The 3! possible permutations with 3 elements:\n";
+    do {
+        cout << arr[0] << " " << arr[1] << " " << arr[2] << "\n";
+    } while (next_permutation(arr, arr + 3));
+ 
+    cout << "After loop: " << arr[0] << ' '
+         << arr[1] << ' ' << arr[2] << '\n';
+ 
+    return 0;
+}
