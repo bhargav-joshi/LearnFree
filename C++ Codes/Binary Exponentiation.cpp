425fc1ed1e06581639b662e80bb4d177c94eec55
mayankmj <81777768+mayankmj@users.noreply.github.com>
2021-10-31 00:46:10 +0530

    Create Binary Exponentiation.cpp

---
+++ C++ Codes/Binary Exponentiation.cpp
@@ -0,0 +1,115 @@
+Binary Exponential algo
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long int lli;
+
+#define all(arr) arr.begin(),arr.end()
+#define f first
+#define s second
+#define debug1(x) cout<<x<<"\n"
+#define debug2(x,y) cout<<x<<" "<<y<<"\n"
+#define debug3(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n"
+#define nl cout<<"\n";
+#define pq priority_queue
+#define inf 0x3f3f3f3f
+#define test cout<<"abcd\n";
+#define pi pair<int,int>
+#define pii pair<int,pi>
+#define pb push_back
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define MOD 1000000007
+#define space ' '
+#define kick(t) cout << "Case #" << t+1 << ":" << endl;
+
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+
+template <typename T>
+void input(vector<T> &arr,lli n) {
+  T temp;
+  for(lli i=0;i<n;i++) cin>>temp, arr.push_back(temp);
+}
+
+template <typename T>
+void output(vector<T> arr) {
+  for(auto x:arr) cout<<x<<" ";
+  cout<<endl;
+}
+
+
+template <typename T>
+void input_set(set<T> &arr,lli n) {
+  T temp;
+  for(lli i=0;i<n;i++) cin>>temp, arr.insert(temp);
+}
+
+lli mul(lli a, lli b) {
+  return (a%MOD*b%MOD)%MOD;
+}
+
+lli power(lli a,lli b) {
+  lli ans = 1;
+  while(b > 0) {
+    if(b&1)
+      ans = mul(ans, a);
+    a = mul(a,a);;
+    b >>= 1;
+  }
+  return ans;
+}
+
+lli binaryExponentiation(lli x,lli n)
+{
+    lli result=1;
+    while(n>0)
+    {
+        if(n % 2 ==1)
+            result=result * x;
+        x=x*x;
+        n=n/2;
+    }
+    return result;
+}
+
+void solve(int testcase) {
+    lli n;
+    cin >> n;
+    lli sum  = binaryExponentiation(2,n);
+    cout<<sum<<endl;
+}
+
+int main() {
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  lli testcases=1;
+  cin >> testcases;
+  for(int testcase=0; testcase<testcases; testcase++) {
+    solve(testcase);
+  }
+}
