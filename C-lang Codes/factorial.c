86499b802855a998a7c6b8c9f88d6c5105d91ee8
vidhidrana <56223752+vidhidrana@users.noreply.github.com>
2021-10-30 11:31:57 +0530

    Factorial using recursion
    Recursive approach could be more efficient rather than traditional one..

--- C-lang Codes/factorial.c
+++ C-lang Codes/factorial.c
@@ -1,12 +1,14 @@
-#include<stdio.h>  
-int main()    
-{    
- int i,fact=1,number;    
- printf("Enter a number: ");    
-  scanf("%d",&number);    
-    for(i=1;i<=number;i++){    
-      fact=fact*i;    
-  }    
-  printf("Factorial of %d is: %d",number,fact);    
-return 0;  
-} 
+#include<stdio.h>
+long int fact(int n) {
+    if (n>=1)
+        return n*fact(n-1);
+    else
+        return 1;
+}
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    printf("Factorial of %d = %ld", n, fact(n));
+    return 0;
+}
