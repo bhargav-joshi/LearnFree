67074220d12723fa2435ebdbbd2254516df82cd3
Mantosh Sharma <56771403+mr-mantosh@users.noreply.github.com>
2021-10-31 23:51:20 +0530

    Comparison

---
+++ C++ Codes/Comparision.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float x = 0.1;
+    if (x == 0.1)
+        printf("IF");
+    else if (x == 0.1f)
+        printf("ELSE IF");
+    else
+        printf("ELSE");
+}
+
+#output : ELSE IF
